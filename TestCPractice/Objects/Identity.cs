aa1d0a3685775c706800e66830445779d1b0f80f
301038409 <301038409@PR-E216-06>
2019-08-14 10:59:42 -0400

    Load of names and lastnames added

--- TestCPractice/Objects/Identity.cs
+++ TestCPractice/Objects/Identity.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Collections.Generic;  
+using System.Collections.Generic;
+using System.IO;
+using System.Diagnostics;
 /*
 Andrey Paramonov
 ID: 301038409
@@ -11,7 +13,7 @@ Descr: This is an Identity class for an application
 */
 namespace TestCPractice.Objects
 {
-   public class Identity
+    public class Identity
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
@@ -20,7 +22,37 @@ namespace TestCPractice.Objects
         List<string> FirstNameList = new List<string>();
         List<string> LastNameList = new List<string>();
 
-         LoadNames
+        private void LoadNames()
+        {
+            using (StreamReader inputStream = new StreamReader(
+         File.Open("Data/firstNames.txt", FileMode.Open)))
+            {
+                string line;
+                int counter = 0;
+                while ((line = inputStream.ReadLine()) != null)
+                {
+                    FirstNameList.Add(line);
+                    counter++;
+                }
+                inputStream.Close();
+                inputStream.Dispose();
 
+            }
+
+            using (StreamReader inputLStream = new StreamReader(
+     File.Open("Data/lastNames.txt", FileMode.Open)))
+            {
+                string line;
+                int counter = 0;
+                while ((line = inputLStream.ReadLine()) != null)
+                {
+                    LastNameList.Add(line);
+                    counter++;
+                }
+                inputLStream.Close();
+                inputLStream.Dispose();
+            }
+        
+        }
     }
 }
