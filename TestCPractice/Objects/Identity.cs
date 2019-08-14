f5fd5a0c35105f04a1247e00ca3af73d0fb468ab
301038409 <301038409@PR-E216-06>
2019-08-14 10:42:58 -0400

    test starts

--- TestCPractice/Objects/Identity.cs
+++ TestCPractice/Objects/Identity.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.Generic;  
 /*
 Andrey Paramonov
 ID: 301038409
@@ -14,6 +15,12 @@ namespace TestCPractice.Objects
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
+
+        List<string> FirstNameList = new List<string>();
+        List<string> LastNameList = new List<string>();
+
+         LoadNames
 
     }
 }
